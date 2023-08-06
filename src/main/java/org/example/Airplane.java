a5d16534f841b79533b9ebf73901ef32df4ddb24
LY <1287432999@qq.com>
2023-08-06 16:28:29 +0800

    YaoLi updates Airplane

--- src/main/java/org/example/Airplane.java
+++ src/main/java/org/example/Airplane.java
@@ -7,23 +7,25 @@ public class Airplane
     private int economySitsNumber;
     private int crewSitsNumber;
 
+
     public Airplane(int airplaneID, String airplaneModel, int businessSitsNumber, int economySitsNumber, int crewSitsNumber)
     {
 
-        if(airplaneModel == null){
-            throw new IllegalArgumentException("airplaneModel must be null.");
+        if (airplaneModel == null || airplaneModel.isEmpty()) {
+            throw new IllegalArgumentException("airplaneModel must not be null or empty.");
         }
-        if(businessSitsNumber < 1 || businessSitsNumber > 300){
+
+        if(businessSitsNumber < 1 || businessSitsNumber > 298){
             throw new IllegalArgumentException("Seat number must be in the range [1, 300].");
         }
-        if(economySitsNumber < 1 || economySitsNumber > 300){
+        if(economySitsNumber < 1 || economySitsNumber > 298){
             throw new IllegalArgumentException("Seat number must be in the range [1, 300].");
         }
-        if(crewSitsNumber < 1 || crewSitsNumber > 300){
+        if(crewSitsNumber < 1 || crewSitsNumber > 298){
             throw new IllegalArgumentException("Seat number must be in the range [1, 300].");
         }
         int totalSitsNumber = businessSitsNumber + economySitsNumber + crewSitsNumber;
-        if(totalSitsNumber < 1 || totalSitsNumber > 300){
+        if(totalSitsNumber < 3 || totalSitsNumber > 300){
             throw new IllegalArgumentException("Seat number must be in the range [1, 300].");
         }
         if(airplaneID <= 0 ){
@@ -43,28 +45,27 @@ public class Airplane
     }
 
     public void setAirplaneID(int airplaneID) {
-        if(airplaneID <= 0 ){
-            throw new IllegalArgumentException("airplaneID must be positive integer.");
-        }
-        else{
+         if (airplaneID <= 0) {
+            throw new IllegalArgumentException("airplaneID must be a positive integer.");
+        } else {
             this.airplaneID = airplaneID;
         }
-
-
     }
+
 
     public String getAirplaneModel() {
         return airplaneModel;
     }
 
     public void setAirplaneModel(String airplaneModel) {
-        if( airplaneModel == null){
-            throw new IllegalArgumentException("airplaneModel must be null.");
-        }
-        else{
-        this.airplaneModel = airplaneModel;
+        if (airplaneModel != null && !airplaneModel.isEmpty()) {
+            this.airplaneModel = airplaneModel;
+        } else {
+            throw new IllegalArgumentException("airplaneModel must not be null or empty.");
         }
     }
+
+
 
 
     public int getBusinessSitsNumber() {
@@ -72,7 +73,7 @@ public class Airplane
     }
 
     public void setBusinessSitsNumber(int businessSitsNumber) {
-        if(businessSitsNumber < 1 || businessSitsNumber > 300){
+        if(businessSitsNumber < 1 || businessSitsNumber > 298){
             throw new IllegalArgumentException("Seat number must be in the range [1, 300].");
         }
         else {
@@ -86,7 +87,7 @@ public class Airplane
     }
 
     public void setEconomySitsNumber(int economySitsNumber) {
-        if(economySitsNumber < 1 || economySitsNumber > 300){
+        if(economySitsNumber < 1 || economySitsNumber > 298){
             throw new IllegalArgumentException("Seat number must be in the range [1, 300].");
         }
         else {
@@ -101,7 +102,7 @@ public class Airplane
     }
 
     public void setCrewSitsNumber(int crewSitsNumber) {
-        if(crewSitsNumber < 1 || crewSitsNumber > 300){
+        if(crewSitsNumber < 1 || crewSitsNumber > 298){
             throw new IllegalArgumentException("Seat number must be in the range [1, 300].");
         }
         else {
